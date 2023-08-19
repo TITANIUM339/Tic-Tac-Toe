57d7977a929541a3a70f31371f4174367eba3e6c
TITANIUM <bobofofo576@gmail.com>
2023-08-19 13:39:12 +0300

    Update game.h

--- game.h
+++ game.h
@@ -18,28 +18,41 @@ typedef struct Coordinate {
 } coordinate;
 
 
+// Prints the game board.
 void print_board();
 
+// Checks for a winner.
 char check_winner(char board[3][3]);
 
+// Moves player.
 int move_player(char player, int x, int y);
 
+// Resets game board.
 void reset_board();
 
+// Checks for free spaces on the game board.
 int check_free_spaces(char board[3][3]);
 
+// Computer makes move using minimax algorithm.
 void computer_move(char computer);
 
+// Prompts the player to select a team.
 team get_player_team();
 
+// Prompts the player for a move based on x, y coordinates.
 coordinate get_player_move();
 
+// Gets input from player, simillar to the input function in python.
 char* input(char* prompt);
 
+// Creates a new board.
 char** create_board();
 
+// Returns a new game board resulting from a move.
 char** move_result(char board[3][3], coordinate move, char computer);
 
+// Deletes a board created using the create_board() function.
 void delete_board(char** board);
 
+// The minimax algorithm.
 int minimax(char board[3][3], char computer);
